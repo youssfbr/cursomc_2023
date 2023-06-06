8d847c4b40c68f7662c110a0b72f63adcc3d7a01
Alisson Youssf <youssfbr@gmail.com>
2023-06-06 14:33:21 -0300

    Testing Category

--- src/main/java/com/github/youssfbr/cursomc/controllers/CategoryController.java
+++ src/main/java/com/github/youssfbr/cursomc/controllers/CategoryController.java
@@ -1,16 +1,26 @@
 package com.github.youssfbr.cursomc.controllers;
 
+import com.github.youssfbr.cursomc.entities.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
     @GetMapping
-    public String list() {
-        return "REST OK";
+    public List<Category> list() {
+
+        List<Category> categories = new ArrayList<>();
+
+        categories.add(new Category(1L, "Informática"));
+        categories.add(new Category(2L, "Escritório"));
+
+        return categories;
     }
-    
+
 }
---
+++ src/main/java/com/github/youssfbr/cursomc/entities/Category.java
@@ -0,0 +1,78 @@
+package com.github.youssfbr.cursomc.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    private void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        updatedAt = Instant.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
